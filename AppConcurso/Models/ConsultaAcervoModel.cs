b14d6cabfa93026c92f9ec49ac7cd4379f674eb9
MarcosEdsonAnerio <MarcosEdsonAnerio@hotmail.com>
2025-07-12 18:57:02 -0400

    SGB.5

---
+++ AppConcurso/Models/ConsultaAcervoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGB_Project.Models
+{
+    [NotMapped]
+    public class ConsultaAcervoModel
+    {
+        public int IdLivro { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public int Estoque { get; set; }
+
+        public bool Disponivel => Estoque > 0;
+    }
+}
